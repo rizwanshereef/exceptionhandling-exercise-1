be277aa11312893e913b03af0911928bf138ee14
rsrizwan77 <rsrizwan77@gmail.com>
2020-09-09 10:02:42 +0530

    Adding program main() exception

---
+++ MainClassException/src/com/stackroute/Main.java
@@ -0,0 +1,17 @@
+package com.stackroute;
+public class Main extends Exception {
+    public Main(String message) {
+        super(message);
+    }
+    public static void main(String[] args) {
+        try{
+            throw new Main("exception occured in main method.");
+        }catch (Exception e){
+            System.out.println(e);
+        }
+        finally {
+            System.out.println("this is a finally block.");
+        }
+        System.out.println("this is after try-catch-finally block.");
+    }
+}
